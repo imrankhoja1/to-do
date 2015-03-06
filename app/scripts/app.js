588d0bfc3971432bc34dea8ef4c542dfe576fbb5
vagrant <vagrant@precise32.(none)>
2015-03-06 00:54:41 +0000

    first commit with yo angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name toDoApp
+ * @description
+ * # toDoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('toDoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
